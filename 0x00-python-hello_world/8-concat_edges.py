a60d11f3b4129a477640fc92a4261ae6f1da4a93
hima890 <hfibrahim90@gmail.com>
2023-11-27 12:43:26 +0200

    Update task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(f"{str[39:55]}", end="")
-print(f"{str[55:66]} {str[107:111]} {str[0:6]}")
+print(f"{str[72:78]} {str[107:111]} {str[0:6]}")
