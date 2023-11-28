8ba1fcdefc7f84bf8f00ba4e5ad8be732be4f062
hima890 <hfibrahim90@gmail.com>
2023-11-28 21:29:25 +0200

    Update task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number < 10:
-        print(f"0{number}, ", end="")
+        print("0{}, ".format(number), end="")
     else:
         if number == 99:
-            print(f"{number}")
+            print("{}".format(number))
         else:
-            print(f"{number}, ", end="")
+            print("{}, ".format(number), end="")
