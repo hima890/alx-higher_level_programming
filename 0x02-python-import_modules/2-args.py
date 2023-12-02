5dc9ac596c81029b884280910457c8934c5a0624
hima890 <hfibrahim90@gmail.com>
2023-12-02 11:36:51 +0200

    Update task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-argLen = len(sys.argv)
-arguments = sys.argv
-if argLen == 1:
-    print("{} arguments.".format(argLen))
-elif argLen == 2:
-    print("1 argument:".format(argLen))
-    print("1: {}".format(arguments[1]))
-elif argLen > 2:
-    print("{} arguments:".format(argLen - 1))
-    for index in range(argLen):
-        if index == 0:
-            continue
-        print("{}: {}".format(index, arguments[index]))
+if __name__ == "__main__":
+    import sys
+    argLen = len(sys.argv)
+    arguments = sys.argv
+    if argLen == 1:
+        print("{} arguments.".format(argLen))
+    elif argLen == 2:
+        print("1 argument:".format(argLen))
+        print("1: {}".format(arguments[1]))
+    elif argLen > 2:
+        print("{} arguments:".format(argLen - 1))
+        for index in range(argLen):
+            if index == 0:
+                continue
+            print("{}: {}".format(index, arguments[index]))
