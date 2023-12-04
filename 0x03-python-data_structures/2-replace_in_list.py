569c83d1bdcbc3d49a66caa06b14eea994d54b91
hima890 <hfibrahim90@gmail.com>
2023-12-04 22:20:13 +0200

    Add task 8-14

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    # Check if idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    # Modify the list by replacing the element at the specified index
+    new_list = my_list[:]
+    new_list[idx] = element
+
+    return new_list
