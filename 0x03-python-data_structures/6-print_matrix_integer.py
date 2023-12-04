86e5cb64c36e73f71b45da21fb970111e8140f65
hima890 <hfibrahim90@gmail.com>
2023-12-04 22:01:25 +0200

    Add task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 def print_matrix_integer(matrix=None):
     if matrix:
         for row in matrix:
-            for column in row:
-                print("{:d}".format(column), end=" ")
+            for index, column in enumerate(row):
+                if index == len(row) - 1:
+                    print('{:d}'.format(column), end='')
+                else:
+                    print('{:d}'.format(column), end=' ')
             print()  # Move to a new line after each row
