235938a2ee93d4f9f0978870a5f2661c77e786bd
hima890 <hfibrahim90@gmail.com>
2023-12-04 17:55:16 +0200

    Update task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        new_string = ""
-        for charchter in my_string:
-            if charchter not in "Cc":
-                new_string += charchter
-            else:
-                continue
-        return (new_string)
+        new_characters = []
+        for character in my_string:
+            if character not in "Cc":
+                new_characters.append(character)
+        return ''.join(new_characters)
     else:
-        return (None)
+        return ""
