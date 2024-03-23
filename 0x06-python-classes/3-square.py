3b6387f0775261c0c14326629e3a5c42d559fad0
hima890 <hfibrahim90@gmail.com>
2024-03-23 18:50:16 +0200

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    This a simple class to demonstrate documentation of classes in python
+    incilding attributes and functions and more using the beast stander.
+
+    Attributes:
+        - size: Un Intger attribute
+    """
+
+    def __init__(self, size=0):
+        """
+        Constructor for Square.
+
+        Parameters:
+        - size (str): Value for size.
+        """
+
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if not self.__size >= 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Public instance method: def area(self):
+        that returns the current square area
+        """
+        return (self.__size * self.__size)
