4b787321b1f56a078a5f17a055354ae9c5de552c
hima890 <hfibrahim90@gmail.com>
2024-03-23 17:13:27 +0200

    Task 0 update 4

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,17 +4,9 @@ class Square:
     This a simple class to to demonstrate documentation.
 
     Attributes:
-    - hight: A integer attribute
-    - width: An integer attribute
     """
 
-    def __init__(self, hight = 10, widht = 10):
+    def __init__(self):
         """
         Constructor for Square.
-
-        Parameters:
-        - hight (int): Value for attribute1 (default is 10).
-        - widht (int): Value for attribute2 (default is 10).
         """
-        self.hight = hight
-        self.width = widht
