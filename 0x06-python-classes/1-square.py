c48a2e7c803fef57f77468d683081aef0727aa06
hima890 <hfibrahim90@gmail.com>
2024-03-23 17:28:11 +0200

    1-squary.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    This a simple class to demonstrate documentation of classes in python
+    incilding attributes and functions and more using the beast stander.
+    """
+    __size = 3
+    pass
+
