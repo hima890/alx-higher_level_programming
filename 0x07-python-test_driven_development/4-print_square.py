50dd42b0ec7b66f06bc83952c26dcddd302b211d
hima890 <hfibrahim90@gmail.com>
2024-04-05 16:17:18 +0200

    Add and update task5 test file

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+A function that printa Squer.
+
+
+"""
+
+
+def print_square(size):
+    """
+    Print a square made of '#' characters.
+
+    Parameters:
+    - size (int): The size length of the square.
+
+    Raises:
+    - TypeError: If size is not an integer or if size is a float.
+    - ValueError: If size is less than 0.
+
+    Returns:
+    - None
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+A function that print text with newlines.
+
+
+"""
+
+
+def text_indentation(text):
+    """
+    Print the input text with indentation based on punctuation marks.
+
+    Parameters:
+    - text (str): The input text to be printed.
+
+    Raises:
+    - TypeError: If text is not a string.
+
+    Returns:
+    - None
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for char in text:
+        print(char, end="")
+        if char in ('.', '?', ':'):
+            print("\n\n", end="")
