2787bcf64fb89db60a4ca7227de7c50a02340a1a
hima890 <hfibrahim90@gmail.com>
2024-04-05 15:19:06 +0200

    Add and update task3 test file

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+A function that print the full name.
+
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print the full name based on the provided first and last names.
+
+    Parameters:
+    - first_name (str): The first name of the person.
+    - last_name (str, optional): The last name of the person.
+        Defaults to an empty string.
+
+    Raises:
+    - TypeError: If first_name is not a string or last_name is not a string.
+
+    Returns:
+    - None
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
