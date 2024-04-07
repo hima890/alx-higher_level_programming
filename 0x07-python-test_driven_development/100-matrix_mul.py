d20b54a67a54a6f2f3c6464d04643fa586d8580c
hima890 <hfibrahim90@gmail.com>
2024-04-07 15:19:12 +0200

    Add task 100 and test case

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+A function that print text with newlines.
+
+
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    Multiply two matrices.
+
+    Parameters:
+    - m_a (list of lists): First matrix
+    - m_b (list of lists): Second matrix
+
+    Returns:
+    - result (list of lists): Resulting matrix after multiplication
+
+    Raises:
+    - TypeError: If m_a or m_b is not a list, or if m_a or m_b is not a list of
+    lists,
+    or if one element of the list of lists is not an integer or a float,
+    or if m_a or m_b is not a rectangle (all rows should be of the same size)
+    or if m_a and m_b can't be multiplied.
+    - ValueError: If m_a or m_b is empty.
+    """
+    # Validate m_a and m_b
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if len(m_a) == 0 or len(m_a[0]) == 0:
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0 or len(m_b[0]) == 0:
+        raise ValueError("m_b can't be empty")
+
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all(isinstance(num, (int, float)) for row in m_b for num in row):
+        raise TypeError("m_b should contain only integers or floats")
+
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+
+    # Perform matrix multiplication
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = [[0 for _ in range(len(m_b[0]))] for _ in range(len(m_a))]
+
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+
+    return result
