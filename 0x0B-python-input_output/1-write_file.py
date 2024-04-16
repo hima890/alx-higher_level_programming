74d7462f47337700f29ad2bd84d2c251812bd8a4
hima890 <hfibrahim90@gmail.com>
2024-04-16 16:39:20 +0200

    Add task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of
+    characters written.
+
+    Args:
+        filename (str): The name of the text file to write to.
+        text (str): The string to write to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+
+    Example:
+        nb_characters = write_file("my_first_file.txt",
+        "This School is so cool!\n")
+        print(nb_characters)  # Output: 29
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        num_chars_written = file.write(text)
+    return num_chars_written
