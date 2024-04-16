e6eba8665be407e64999e65f913946106e758b2f
hima890 <hfibrahim90@gmail.com>
2024-04-16 16:44:46 +0200

    Add task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be converted to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+
+    Example:
+        my_list = [1, 2, 3]
+        s_my_list = to_json_string(my_list)
+        print(s_my_list)  # Output: "[1, 2, 3]"
+    """
+    import json
+    return json.dumps(my_obj)
