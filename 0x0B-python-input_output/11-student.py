e767fa56364e0ae4d6495ccd35392619625cff21
hima890 <hfibrahim90@gmail.com>
2024-04-16 17:11:12 +0200

    Add task 11 12

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,58 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+class Student:
+    """
+    Defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list of str, optional): A list of attribute names
+            to retrieve.
+                If None, retrieve all attributes.
+
+        Returns:
+            dict: A dictionary containing the specified attributes
+            of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)
+            }
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance with
+        the provided dictionary.
+
+        Args:
+            json (dict): A dictionary containing attribute
+            names and values.
+
+        Returns:
+            None
+        """
+        for attr, value in json.items():
+            setattr(self, attr, value)
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal’s triangle of n.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list: A list of lists representing Pascal’s triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for row in range(n):
+        current_row = [1] * (row + 1)
+        for col in range(1, len(current_row) - 1):
+            value1 = triangle[row - 1][col]
+            value2 = triangle[row - 1][col - 1]
+            current_row[col] = value1 + value2
+
+        triangle.append(current_row)
+
+    return triangle
