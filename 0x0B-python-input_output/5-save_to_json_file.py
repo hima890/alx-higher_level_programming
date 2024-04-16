55bcf7dd5f9e93558de25b1e91e3c8b9648b08f9
hima890 <hfibrahim90@gmail.com>
2024-04-16 16:49:03 +0200

    Add task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to a text file using a JSON representation.
+
+    Args:
+        my_obj: The object to be serialized and written to the file.
+        filename (str): The name of the text file to write to.
+
+    Returns:
+        None
+
+    Example:
+        my_list = [1, 2, 3]
+        save_to_json_file(my_list, "my_list.json")
+    """
+    import json
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
