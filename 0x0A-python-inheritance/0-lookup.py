f90f0c7ea2b856ebd8df6f0ad05c6cebfe17acd0
hima890 <hfibrahim90@gmail.com>
2024-04-16 09:30:51 +0200

    Add task 0, gitignore

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Get the object attributes and methods
+"""
+
+def lookup(obj):
+    """
+    Function to get all object attributes and methods,
+    using the dir method
+
+    Args:
+    obj: Python object
+
+    Return: A list content all the attributes and methods
+    """
+    return list(dir(obj))
