316fd83be938ac4f898f5ed5da3ac4356e5c3b12
hima890 <hfibrahim90@gmail.com>
2024-04-16 17:05:30 +0200

    Add task 7, 8, 9, 10

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,61 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+import sys
+from os import path
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to a text file using a JSON representation.
+
+    Args:
+        my_obj: The object to be serialized and written to the file.
+        filename (str): The name of the text file to write to.
+
+    Returns:
+        None
+
+    Example:
+        my_list = [1, 2, 3]
+        save_to_json_file(my_list, "my_list.json")
+    """
+    import json
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file to read from.
+
+    Returns:
+        object: The Python object represented by the JSON data in the file.
+
+    Example:
+        my_list = load_from_json_file("my_list.json")
+    """
+    import json
+    with open(filename, mode='r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+# Filename for the JSON file
+filename = "add_item.json"
+
+# Check if the file exists, if not create it
+if not path.exists(filename):
+    save_to_json_file([], filename)
+
+# Load the existing list from the file
+my_list = load_from_json_file(filename)
+
+# Add arguments to the list
+my_list.extend(sys.argv[1:])
+
+# Save the updated list to the file
+save_to_json_file(my_list, filename)
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a Class.
+
+    Returns:
+        dict: A dictionary representing the object in a serializable format.
+
+    Example:
+        m = MyClass("John")
+        mj = class_to_json(m)
+        print(mj)  # Output: {'name': 'John', 'number': 89}
+    """
+    json_dict = {}
+    for attr, value in obj.__dict__.items():
+        # Check if the attribute value is a serializable type
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[attr] = value
+    return json_dict
