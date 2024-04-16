2ef022390ce11d0540f448060d98b5de2eb130e3
hima890 <hfibrahim90@gmail.com>
2024-04-16 16:50:43 +0200

    Add task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file to read from.
+
+    Returns:
+        object: The Python object represented by the JSON data in the file.
+
+    Example:
+        my_list = load_from_json_file("my_list.json")
+    """
+    import json
+    with open(filename, mode='r', encoding='utf-8') as file:
+        return json.load(file)
