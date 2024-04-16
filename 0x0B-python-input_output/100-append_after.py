bd81cfb6f1b50b8644d90e4714a063f51bcd2387
hima890 <hfibrahim90@gmail.com>
2024-04-16 17:14:28 +0200

    Add task 13, 14

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file after each line containing a
+    specific string.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for in each line.
+        new_string (str): The line of text to insert after each line
+        containing the search string.
+
+    Returns:
+        None
+    """
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,1 +1,51 @@
 #!/usr/bin/python3
+"""Input output in python"""
+
+
+import sys
+
+
+def print_metrics(total_size, status_counts):
+    """
+    Prints the computed metrics.
+
+    Args:
+        total_size (int): Total file size.
+        status_counts (dict): Dictionary containing status code counts.
+
+    Returns:
+        None
+    """
+    print("File size: {}".format(total_size))
+    for code in sorted(status_counts.keys()):
+        print("{}: {}".format(code, status_counts[code]))
+
+
+def main():
+    """
+    Main function to read stdin line by line and compute metrics.
+    """
+    total_size = 0
+    status_counts = {"200": 0, "301": 0, "400": 0, "401": 0,
+                     "403": 0, "404": 0, "405": 0, "500": 0}
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            split_line = line.split(" ")
+            total_size += int(split_line[-1])
+            status_code = split_line[-2]
+            if status_code in status_counts:
+                status_counts[status_code] += 1
+
+            if line_count % 10 == 0:
+                print_metrics(total_size, status_counts)
+
+    except KeyboardInterrupt:
+        print_metrics(total_size, status_counts)
+        raise
+
+
+if __name__ == "__main__":
+    main()
