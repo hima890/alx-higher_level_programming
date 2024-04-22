da8ab7ba56f706bdfd5dcf189f4bcf02353a67ab
hima890 <hfibrahim90@gmail.com>
2024-04-22 18:52:56 +0200

    Add task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,46 +1,58 @@
 #!/usr/bin/python3
-
 from models.base import Base
-
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width  # private attribute
-        self.__height = height  # private attribute
-        
-        self.__x = x  # private attribute
-        self.__y = y  # private attribute
-        
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
     @property
-    def private_width(self):
+    def width(self):
         return self.__width
-    
-    @private_width.setter
-    def private_width(self, value):
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
-    
+
     @property
-    def private_height(self):
+    def height(self):
         return self.__height
-    
-    @private_height.setter
-    def private_height(self, value):
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
-    
-    
+
     @property
-    def private_x(self):
+    def x(self):
         return self.__x
-    
-    @private_x.setter
-    def private_x(self, value):
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
-        
+
     @property
-    def private_y(self):
+    def y(self):
         return self.__y
-    
-    @private_y.setter
-    def private_y(self, value):
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
