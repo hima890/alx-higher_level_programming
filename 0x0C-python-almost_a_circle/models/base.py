0d903441fc428b1c782ac168b2f2c5a07aafaf1a
hima890 <hfibrahim90@gmail.com>
2024-04-22 19:41:58 +0200

    Add task 12

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import json
 
 class Base:
     __nb_objects = 0
@@ -9,3 +10,9 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
