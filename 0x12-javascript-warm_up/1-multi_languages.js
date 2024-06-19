428fcef64b092e4ddf9e712661687850a6677ff8
hima890 <hfibrahim90@gmail.com>
2024-06-19 12:55:48 +0200

    Add/Task-1-0

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 // Write a script that prints 3 lines:
 const firstLine = 'C is fun';
 const secondLine = 'Python is cool';
-const thirdLine = 'JavaScript is amazing'
-console.log(firstLine)
-console.log(secondLine)
-console.log(thirdLine)
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
