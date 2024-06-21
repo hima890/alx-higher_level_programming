91cffa9ceb9e7a3bac997fd068ff00c1587d2349
hima890 <hfibrahim90@gmail.com>
2024-06-21 15:41:15 +0200

    Add/All-tasks

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+exports.converter = function (base) {
+  // Define the recursive function within the main function
+  function convert (num) {
+    // Check if num is zero, which marks the base case
+    if (num === 0) return ''; // Return empty string to terminate recursion
+
+    // Recursively calculate the remainder and convert the quotient
+    return convert(Math.floor(num / base)) + (num % base).toString(base);
+  }
+
+  // Return the inner function to be used as the converter
+  return convert;
+};
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Creates an empty object if w or h is not a positive integer
+    }
+  }
+}
+
+module.exports = Rectangle;
