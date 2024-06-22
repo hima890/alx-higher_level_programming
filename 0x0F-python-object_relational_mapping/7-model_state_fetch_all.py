8506223d7526d9f81445b13df9c521cb21752475
hima890 <hfibrahim90@gmail.com>
2024-06-22 12:12:20 +0200

    Add/task-7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""
+Script to list all State objects from the database
+hbtn_0e_6_usa using SQLAlchemy.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    # Create database connection URL
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username,
+        password,
+        database_name
+    )
+
+    # Create engine
+    engine = create_engine(db_url)
+
+    # Create a sessionmaker
+    Session = sessionmaker(bind=engine)
+
+    # Create session
+    session = Session()
+
+    # Query all State objects sorted by id
+    states = session.query(State).order_by(State.id).all()
+
+    # Print states in the desired format
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close session
+    session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Python file that contains the class definition of a State and an instance
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class that inherits from Base
+
+    Attributes:
+        id: Id state
+        name: Name of state
+    """
+    # Linke the class to the crossbasnding table
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
