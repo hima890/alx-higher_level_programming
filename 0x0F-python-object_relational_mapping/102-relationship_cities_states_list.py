ff51984df40bf1b46311854a294febe02cb3e055
hima890 <hfibrahim90@gmail.com>
2024-06-23 07:33:37 +0200

    Add/task-16-17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""
+    Module that performs MySQL query through MySQLAlchemy.
+"""
+
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
 import sys
-"""
-    Module that performs MySQL query through MySQLAlchemy.
-"""
-
 
 if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
