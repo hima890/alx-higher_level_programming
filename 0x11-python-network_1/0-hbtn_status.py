e0fb1d668d52b8ba6c5fcf0078d3e78bf3e4c71f
hima890 <hfibrahim90@gmail.com>
2024-07-05 10:59:45 +0200

    Add/Task-0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+# Code should not be executed when imported
+if __name__ == "__main__":
+    import urllib.request
+
+    # Request the URL
+    url = 'https://alx-intranet.hbtn.io/status'
+    request = urllib.request.Request(url)
+    # Read the response countent
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        # Output the response body
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
