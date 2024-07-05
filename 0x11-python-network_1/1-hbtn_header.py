b6c9ea59c07777c4b6e704bc5e55d06e96a7de5f
hima890 <hfibrahim90@gmail.com>
2024-07-05 11:27:05 +0200

    Add/Task-2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""A script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found in
+the header of the response."""
+
+# Code should not be executed when imported
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    # Request the URL
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    # Read the response headers
+    with urllib.request.urlopen(request) as response:
+        headers = dict(response.getheaders())
+        # Output the response body
+        print("{}".format(headers.get("X-Request-Id")))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""A script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found in
+the header of the response."""
+
+# Code should not be executed when imported
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Prepare the data for the POST request
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    # Send the POST request
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read()
+
+    # Display the response body decoded in utf-8
+    print(body.decode('utf-8'))
